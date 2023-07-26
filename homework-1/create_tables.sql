cf981285e5064a2cd8b03bc9ff13212b157c105c
Зенченко Евгений <zenchenko_es@outlook.com>
2023-07-26 20:02:57 +0700

    end homework_BD_1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -11,9 +11,9 @@ CREATE table employees
 	employee_id int Primary key,
 	first_name varchar(100) NOT NULL,
 	last_name varchar(100) NOT NULL,
-	title text,
-	birth_date date,
-	notes text
+	title text NOT NULL,
+	birth_date date NOT NULL,
+	notes text NOT NULL
 );
 
 CREATE table orders
@@ -21,7 +21,7 @@ CREATE table orders
 	order_id int Primary key,
 	customer_id varchar(10) REFERENCES customers(customer_id) NOT NULL,
 	employee_id int REFERENCES employees(employee_id) NOT NULL,
-	order_date date,
+	order_date date NOT NULL,
 	ship_city varchar(100) NOT NULL
 
 );
